036152232e8d10bb2724acf4d2692f62082c3e00
AYUSH JHA <ayushj383@gmail.com>
2022-01-21 17:14:34 +0530

    bg

--- public/landinganimate.js
+++ public/landinganimate.js
@@ -5,14 +5,18 @@ var typed = new Typed(".typing", {
     loop: true
 });
 
-
-var counter = 1; //instantiate a counter
-var maxImage = 4; //the total number of images that are available
-setInterval(function() {
-    document.querySelector('header').style.backgroundImage = "url('img/bg/" + (counter + 1) + ".jpg')";
-    if (counter + 1 == maxImage) {
-        counter = 0; //reset to start
-    } else {
-        ++counter; //iterate to next image
-    }
-}, 5000);
+var images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+    $(function () {
+        var i = 0;
+        $("#section-1").css("background-image", "url(img/bg/" + images[i] + ")");
+        setInterval(function () {
+            i++;
+            if (i == images.length) {
+                i = 0;
+            }
+            $("#section-1").fadeOut(2, function () {
+                $(this).css("background-image", "url(img/bg/" + images[i] + ")");
+                $(this).fadeIn(2);
+            });
+        }, 5000);
+    });
