f99890ff278cfc70d5ee55b43ce69c40a2554ccf
AYUSH JHA <ayushj383@gmail.com>
2022-01-21 12:24:46 +0530

    Added trigger for bg change

--- landinganimate.js
+++ landinganimate.js
@@ -4,3 +4,15 @@ var typed = new Typed(".typing", {
     backSpeed: 30,
     loop: true
 });
+
+
+var counter = 1; //instantiate a counter
+var maxImage = 4; //the total number of images that are available
+setInterval(function() {
+    document.querySelector('main').style.backgroundImage = "url('img/bg/" + (counter + 1) + ".jpg')";
+    if (counter + 1 == maxImage) {
+        counter = 0; //reset to start
+    } else {
+        ++counter; //iterate to next image
+    }
+}, 10000);
